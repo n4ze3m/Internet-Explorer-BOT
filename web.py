56eb5e3011aa3ccf9393a8e73fbca1b6681389dc
NazeemNato <nazeemnob18@gmail.com>
2020-04-24 07:31:28 +0530

    newslinks

--- web.py
+++ web.py
@@ -5,3 +5,13 @@
 # https://talksport.com/football/page/2181/
 
 #https://www.fourfourtwo.com/news/page/15180 from 8000
+
+# https://www.tribalfootball.com/news?page=3500 to 3000
+
+# https://www.football-italia.net/news?page=3800 from 2800
+
+# Transfer website
+
+# https://www.caughtoffside.com/tags/transfer-rumours/page/2500/ to 4150
+
+# https://www.tribalfootball.com/transfers?page=2000 to 3817
