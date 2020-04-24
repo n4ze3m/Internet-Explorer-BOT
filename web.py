fc46bd2b380e0f613263ed6d883b3d0a1d292b25
NazeemNato <nazeemnob18@gmail.com>
2020-04-24 06:52:36 +0530

    newslinks

--- web.py
+++ web.py
@@ -1,1 +1,7 @@
-# Hello World
+# https://www.101greatgoals.com/news/page/3501/
+
+#https://www.mirror.co.uk/sport/football/news/?pageNumber=4000 to 7400
+
+# https://talksport.com/football/page/2181/
+
+#https://www.fourfourtwo.com/news/page/15180 from 8000
